897b4380f4cbf6150e6b5c963d46d715c188b459
samarth3394 <samarthchoudhar56@gmail.com>
2026-06-17 00:01:33 +0530

    Fix Hugging Face OOM by setting Celery concurrency to 1

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ celery -A tasks.celery_app beat --loglevel=info &
 
 # Start Celery worker in the background
 # We use gevent pool because we are on Linux/Docker
-celery -A tasks.celery_app worker --loglevel=info --concurrency=2 &
+celery -A tasks.celery_app worker --loglevel=info --concurrency=1 &
 
 # Start the Flask app using Waitress on port 7860
 python -c "from waitress import serve; from app import app; print('Starting server on port 7860...'); serve(app, host='0.0.0.0', port=7860)"
